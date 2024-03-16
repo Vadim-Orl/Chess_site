3f309153f54a78f711d1d5c96d21b229f9940f46
vad-orl <vad-orl90@mail.ru>
2024-03-16 20:06:17 +0300

    исправлен header background

--- yandex/source/js/main.js
+++ yandex/source/js/main.js
@@ -1,25 +1,14 @@
 import Gallary from "./gallery-lib/gallery"
 
-// Window.resize(function() {
-
-// if(window.innerWidth < 1200) {
-//   runGallety1();
-// }
-
-// window.addEventListener('resize', runGallety1);
-
-// function runGallety1() {
-  // if(window.innerWidth < 1200) {
-
 new Gallary(document.getElementById('gallery-1'), document.getElementById('gallery-1-nav'), {
-      margin: 20,
-      hasTimer: true,
-      hasToggle: true,
-      hasToggleText: false,
-      isSliderNotRound: true,
-      breakpoints: { starting: 375, ending: 1220 }
-    })
-// }
+    margin: 20,
+    hasTimer: true,
+    hasToggle: true,
+    hasToggleText: false,
+    isSliderNotRound: true,
+    breakpoints: { starting: 375, ending: 1220 }
+  })
+
 new Gallary(document.getElementById('gallery-2'), document.getElementById('gallery-2-nav'), {
   margin: 20,
   hasTimer: false,
@@ -27,9 +16,3 @@ new Gallary(document.getElementById('gallery-2'), document.getElementById('galle
   hasToggleText: true,
   isSliderNotRound: true,
 })
-
-// t = null
-// });
-
-
-console.log('hello')
